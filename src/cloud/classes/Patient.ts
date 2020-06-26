7a58618cd4aedaeef0caf9ee299517cea295cad4
Santiago Semhan <santiagosemhan@gmail.com>
2020-06-26 10:26:47 -0300

    Patient beforeSave public read acl

--- src/cloud/classes/Patient.ts
+++ src/cloud/classes/Patient.ts
@@ -4,6 +4,15 @@ class Patient extends Base {
   constructor() {
     super(Patient.name);
   }
+
+  static async beforeSave(request: Parse.Cloud.BeforeSaveRequest) {
+    await super.beforeSave(request);
+    const { object } = request;
+    const acl = object.getACL();
+    if (acl) {
+      acl.setPublicReadAccess(true);
+    }
+  }
 }
 
 export default Patient;
